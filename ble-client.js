75b917d7d892f4ccb08cf5d934b8bae01ba8e372
LittleHydron <little.hydron@gmail.com>
2026-01-21 04:14:10 +0200

    fixing stuff

--- ble-client.js
+++ ble-client.js
@@ -40,6 +40,11 @@ export class BleClient {
         // --- ГОЛОВНА МАГІЯ ТУТ ---
         this.characteristic.addEventListener('characteristicvaluechanged', (event) => {
             const chunk = new Uint8Array(event.target.value.buffer);
+            const hexString = Array.from(chunk)
+                .map(byte => byte.toString(16).padStart(2, '0').toUpperCase())
+                .join(' ');
+
+            console.log(`📡 Отримано пакет (${chunk.length} B):`, hexString);
             
             // Проходимо по кожному байту
             for (let i = 0; i < chunk.length; i++) {
